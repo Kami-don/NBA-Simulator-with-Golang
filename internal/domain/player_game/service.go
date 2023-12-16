a11cec681e2014ac1897ffd249f13ce94e51c841
Kami-don <bigoodmorning1990@gmail.com>
2023-12-16 13:51:45 +0300

    FEAT: PlayerGameInfo is done

--- internal/domain/player_game/service.go
+++ internal/domain/player_game/service.go
@@ -1,1 +1,42 @@
 package playergame
+
+import (
+	"context"
+
+	"github.com/Furkan-Gulsen/NBA-Simulator-with-Golang/internal/entities"
+)
+
+type Service struct {
+	repo RepositoryI
+}
+
+func NewService(repo RepositoryI) *Service {
+	return &Service{
+		repo: repo,
+	}
+}
+
+func (s *Service) Get(ctx context.Context, id string) (*entities.PlayerGameInfo, error) {
+	return s.repo.Get(ctx, id)
+}
+
+func (s *Service) GetAll(ctx context.Context) ([]*entities.PlayerGameInfo, error) {
+	return s.repo.GetAll(ctx)
+}
+
+func (s *Service) Create(ctx context.Context, p *entities.PlayerGameInfo) error {
+	return s.repo.Create(ctx, p)
+}
+
+func (s *Service) Update(ctx context.Context, p *entities.PlayerGameInfo) error {
+	return s.repo.Update(ctx, p)
+}
+
+func (s *Service) Delete(ctx context.Context, id string) error {
+
+	return s.repo.Delete(ctx, id)
+}
+
+func (s *Service) GetAssistLeaders(ctx context.Context) (*entities.PlayerGameInfo, error) {
+	return s.repo.GetAssistLeaders(ctx)
+}
